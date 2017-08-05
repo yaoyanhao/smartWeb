c6fdba2f5f174947ebbea295f792477095957055
vector01.yao <vector01.yao@vipshop.com>
2017-08-05 21:33:25 +0800

    删除无用文件

--- src/main/java/com/vector/smartWeb/UserInterface.java
+++
@@ -1,8 +0,0 @@
-package com.vector.smartWeb;
-
-/**
- * Created by vector01.yao on 2017/8/1.
- */
-public interface UserInterface {
-    void doSomeThing();
-}
--- src/main/java/com/vector/smartWeb/aop/Greeting.java
+++
@@ -1,8 +0,0 @@
-package com.vector.smartWeb.aop;
-
-/**
- * Created by vector01.yao on 2017/8/2.
- */
-public interface Greeting {
-    void sayHello(String name);
-}
